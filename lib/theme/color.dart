5d2ae4598a34bd59f267fdaa94243faf58cfe94f
Viet Hoa <viethoa24py@gmail.com>
2022-06-07 12:43:54 +0700

    change color

--- lib/theme/color.dart
+++ lib/theme/color.dart
@@ -3,16 +3,16 @@ import 'package:flutter/material.dart';
 const primary = Color(0xFFe6b56c);
 const secondary = Color(0xFFe96561);
 
-const mainColor = Color(0xFF262626);
-const darker = Color(0xFF1C1E21);
-const appBgColor = Color(0xFF353E3B);
-const appBarColor = Colors.white70;
-const bottomBarColor = Colors.white70;
-const inActiveColor = Colors.white70;
-const shadowColor = Colors.black;
-const textBoxColor = Colors.black;
-const textColor = Color(0xAB2F2C2C);
-const labelColor = Color(0xFFF8F8F8);
+const mainColor = Color(0xFF000000);
+const darker = Color(0xFF3E4249);
+const appBgColor = Color(0xFFF7F7F7);
+const appBarColor = Color(0xFFF7F7F7);
+const bottomBarColor = Colors.white;
+const inActiveColor = Colors.grey;
+const shadowColor = Colors.black87;
+const textBoxColor = Colors.white;
+const textColor = Color(0xFF333333);
+const labelColor = Color(0xFF8A8989);
 
 const actionColor = Color(0xFFe54140);
 const buttonColor = Color(0xFFcdacf9);
